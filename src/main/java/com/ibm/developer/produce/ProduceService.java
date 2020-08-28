d8b08512bb07ce534e638a7e1446bf678c96a283
Billy Korando <wkorando@gmail.com>
2020-08-28 15:26:56 -0500

    Commenting out correct lines :/

--- src/main/java/com/ibm/developer/produce/ProduceService.java
+++ src/main/java/com/ibm/developer/produce/ProduceService.java
@@ -12,16 +12,16 @@ public class ProduceService {
 	}
 
 	public Produce addNewProduce(Produce produce) {
-		if (produce.getName().isEmpty() || !(produce.getQuantity() > 0)) {
-			throw new ClientException("Missing required value!");
-		}
+//		if (produce.getName().isEmpty() || !(produce.getQuantity() > 0)) {
+//			throw new ClientException("Missing required value!");
+//		}
 		return repo.save(produce);
 	}
 
 	public Iterable<Produce> findProduceByName(String name) {
-//		if (name.matches("[\\W]+")) {
-//			throw new ClientException("Produce name must be alpha numeric!");
-//		}
+		if (name.matches("[\\W]+")) {
+			throw new ClientException("Produce name must be alpha numeric!");
+		}
 		return repo.findByName(name);
 	}
 }
