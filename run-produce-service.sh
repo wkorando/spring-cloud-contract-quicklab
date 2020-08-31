e573df938d81124939a4c5b005999ebb72b70a00
Billy Korando <wkorando@gmail.com>
2020-08-31 14:43:21 -0500

    Testing out something with contracts

--- run-produce-service.sh
+++ run-produce-service.sh
@@ -1,7 +1,8 @@
 docker run --rm \
--e "STUBRUNNER_IDS=com.ibm.developer:produce-service:+:9876" \
+-e "STUBRUNNER_IDS=produce-service:9876" \
+-e "STUBRUNNER_REPOSITORY_ROOT=file://contracts" \
 -e "STUBRUNNER_STUBS_MODE=LOCAL" \
--v "${HOME}/.m2/:/root/.m2:ro" \
+-e "STUBRUNNER_GENERATE_STUBS=TRUE" \
 -p "8083:8083" \
 -p "9876:9876" \
 springcloud/spring-cloud-contract-stub-runner:2.2.4.RELEASE
