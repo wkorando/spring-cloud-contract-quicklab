30ff461e5412438cc6edfe514d7611632613f7e7
Billy Korando <wkorando@gmail.com>
2020-08-31 15:03:23 -0500

    Trying something again

--- run-produce-service.sh
+++ run-produce-service.sh
@@ -1,6 +1,6 @@
 docker run --rm \
 -e "STUBRUNNER_IDS=produce-service:9876" \
--e "STUBRUNNER_REPOSITORY_ROOT=file://contracts" \
+-e "STUBRUNNER_REPOSITORY_ROOT=git://github.com/wkorando/spring-cloud-contract-quicklab.git" \
 -e "STUBRUNNER_STUBS_MODE=LOCAL" \
 -e "STUBRUNNER_GENERATE_STUBS=TRUE" \
 -p "8083:8083" \
